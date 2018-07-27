fa75554e9831251490a1bb20328736c99396e939
jiradeto <jiradet.jd@gmail.com>
2018-07-27 17:02:09 +0700

    add search class

--- sort/bubble-sort/Main.java
+++ sort/bubble-sort/Main.java
@@ -3,13 +3,15 @@
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = {38, 65, 84, 72, 12, 9};
-		int[] result = bubbleSort(arr);
+		int[] result = bubbleSort
+		(arr);
 		for(int i=0; i < arr.length; i++){
 			System.out.println(" >> "+ arr[i]);
 		}
 	}
 
-	public static int[] bubbleSort(int[] arr){
+	public static int[] bubbleSort
+	(int[] arr){
 		return arr;	
 	}
 }
