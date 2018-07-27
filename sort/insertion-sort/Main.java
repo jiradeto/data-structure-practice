e0f40fc4a1ba7e55bb27fdc79bb28604d0f45616
Jiradet Ounjai <jiradet.dpxecommerce@gmail.com>
2018-07-27 16:49:13 +0700

    first commit

---
+++ sort/insertion-sort/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+		int[] arr = {38, 65, 84, 72, 12, 9};
+		int[] result = insertionSort(arr);
+		for(int i=0; i < arr.length; i++){
+			System.out.println("number >>"+ arr[i]);
+		}
+	}
+
+	public static int[] insertionSort(int[] arr){
+		return arr;	
+	}
+}
