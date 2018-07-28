ea7c656f9d3cac7b03b743032b80bae075aa06f4
jiradeto <jiradet.jd@gmail.com>
2018-07-29 01:07:51 +0700

    reformat

--- sort/quick-sort/Main.java
+++ sort/quick-sort/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	
 	public static void printArray(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " | ");
+            System.out.print(arr[i] + " | ");
 		}
 	}
    
