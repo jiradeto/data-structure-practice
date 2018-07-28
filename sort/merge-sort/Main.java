1aacf12d712202d6a0758d07b628a16f49127f22
jiradeto <jiradet.jd@gmail.com>
2018-07-29 01:08:04 +0700

    reformat

--- sort/merge-sort/Main.java
+++ sort/merge-sort/Main.java
@@ -12,11 +12,11 @@ public class Main {
 	
 	public static void printArray(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " | ");
+            System.out.print(arr[i] + " | ");
 		}
 	}
 
-		public static int[] mergesort(int[] arr, int left, int right) {
+    public static int[] mergesort(int[] arr, int left, int right) {
         if (left < right) {
             int middle = (left + right) / 2;
             mergesort(arr, left, middle);
